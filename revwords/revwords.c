7881b67747a1f4c3c45b9760c739ec7f3602040a
Dmitry Tomp <dmitrytomp@gmail.com>
2015-03-17 22:16:17 +0000

    Fixed last word handling

--- revwords/revwords.c
+++ revwords/revwords.c
@@ -8,27 +8,34 @@
 
 char buf[BUFFER_SIZE];
 
+void reverse(char *buf, int len) {
+    for (int j = 0; j < len / 2; j++) {
+        char bj = buf[j];
+        buf[j] = buf[len - j - 1];
+        buf[len - j - 1] = bj;
+    }
+    write_(STDOUT_FILENO, buf, len);
+}
+
+const char DELIMITER = ' ';
+
 int main() {
     int prevPos = 0;
     ssize_t readCount;
     while (true) {
-        readCount = read_until(STDIN_FILENO, buf + prevPos, BUFFER_SIZE - prevPos, ' ');
+        readCount = read_until(STDIN_FILENO, buf + prevPos, BUFFER_SIZE - prevPos, DELIMITER);
         if (readCount == -1) {
             return errno;
         }
         if (readCount == 0) {
-            write_(STDOUT_FILENO, buf, prevPos);
+            reverse(buf, prevPos);
             break;
         }
         int last = 0;
         for (int i = 0; i < prevPos + readCount; i++) {
             if (buf[i] == ' ') {
-                for (int j = last; j < (last + i) / 2; j++) {
-                    char bj = buf[j];
-                    buf[j] = buf[i - 1 - j + last];
-                    buf[i - 1 - j + last] = bj;
-                }
-                write_(STDOUT_FILENO, buf + last, i - last + 1);
+                reverse(buf + last, i - last);
+                write_(STDOUT_FILENO, &DELIMITER, 1);
                 last = i + 1;
             }
         }
