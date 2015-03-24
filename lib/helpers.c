1ad3f8f13163400a7800b9970fd78015d26c38b8
Dmitry Tomp <dmitrytomp@gmail.com>
2015-03-24 20:53:12 +0300

    Implemented spawn function

--- lib/helpers.c
+++ lib/helpers.c
@@ -2,6 +2,12 @@
 #include "unistd.h"
 #include "errno.h"
 #include "stdbool.h"
+#include "sys/types.h"
+#include "sys/wait.h"
+#include "sys/stat.h"
+#include "string.h"
+#include "memory.h"
+#include "stdlib.h"
 
 ssize_t read_(int fd, void *buf, size_t count) {
     ssize_t sum = 0, readResult;
@@ -46,4 +52,48 @@ ssize_t read_until(int fd, void *buf, size_t count, char delimiter) {
         sum += result;
     } while (!spaceEncountered && sum < count);
     return sum;
+}
+
+int spawn(const char *file, char* const argv[]) {
+    int fileLen = strlen(file);
+    char *PATH = getenv("PATH");
+    if (PATH == 0)
+        return -1;
+    char *fullName = calloc(2 * strlen(PATH), sizeof(char));
+    char *last = PATH;
+    struct stat buf;
+    for (;; PATH++) {
+        if (*PATH == ':' || *PATH == '\0') {
+            strncpy(fullName, last, PATH - last);
+            fullName[PATH - last] = '/';
+            strncpy(fullName + (PATH - last) + 1, file, fileLen);
+            fullName[PATH - last + 1 + fileLen] = 0;
+            stat(fullName, &buf);
+            if (S_ISREG(buf.st_mode)) {
+                break;
+            }
+            last = PATH + 1;
+        }
+        if (*PATH == '\0') {
+            break;
+        }
+    }
+    if (S_ISREG(buf.st_mode)) {
+        pid_t child = fork();
+        if (child == -1) {
+            free(fullName);
+            return child;
+        }
+        if (child == 0) {
+            execv(fullName, argv);
+            int failure = errno;
+            free(fullName);
+            _exit(failure);
+        }
+        int c;
+        waitpid(child, &c, 0);
+        free(fullName);
+        return WEXITSTATUS(c);
+    }
+    return -1;
 }
